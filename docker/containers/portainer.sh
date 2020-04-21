b19a2edf2292b7d5f16f211b9e1c661ceb9b6b22
Nick Schlobohm <nschlobohm@willowgreengroup.com.au>
2020-04-21 16:49:58 +1000

    Create portainer.sh

---
+++ docker/containers/portainer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# willowgreengroup/devops-scripts
+# https://github.com/willowgreengroup/devops-scripts/
+portainerDir="/srv/portainer"
+mkdir -p $portainerDir
+sudo docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v $portainerDir:/data --name portainer portainer/portainer
