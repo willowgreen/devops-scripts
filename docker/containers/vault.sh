a5877c398248a696d9dd615ef55ff47ac60bcbdf
Nick Schlobohm <nschlobohm@willowgreengroup.com.au>
2020-04-21 23:01:19 +1000

    Create vault.sh

---
+++ docker/containers/vault.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# willowgreengroup/devops-scripts
+# https://github.com/willowgreengroup/devops-scripts/
+vaultConfigDir=/srv/vault/config
+mkdir -p $vaultConfigDir
+#
+vaultFileDir=/srv/vaule/file
+mkdir -p $vaultFileDir
+#
+sudo docker run -d \
+-p 8200:8200 \
+-v $vaultConfigDir:/vault/config \
+-v $vaultFileDir:/vault/file \
+--restart always \
+--name vault
+vault
