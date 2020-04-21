2072dcbfcf57a138be595b741e828e4380e5fa6c
Nick Schlobohm <nschlobohm@willowgreengroup.com.au>
2020-04-21 22:43:18 +1000

    Convert `docker run` commands from single-line to multi-line

--- docker/containers/cassandra.sh
+++ docker/containers/cassandra.sh
@@ -3,4 +3,10 @@
 # https://github.com/willowgreengroup/devops-scripts/
 cassandraDir="/srv/cassandra"
 mkdir -p $cassandraDir
-sudo docker run -d -p 9042:9042 -v $cassandraDir:/var/lib/cassandra --restart always --name cassandra cassandra
+#
+sudo docker run -d \
+-p 9042:9042 \
+-v $cassandraDir:/var/lib/cassandra \
+--restart always \
+--name cassandra \
+cassandra
--- docker/containers/portainer.sh
+++ docker/containers/portainer.sh
@@ -3,4 +3,11 @@
 # https://github.com/willowgreengroup/devops-scripts/
 portainerDir="/srv/portainer"
 mkdir -p $portainerDir
-sudo docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v $portainerDir:/data --restart always --name portainer portainer/portainer
+#
+sudo docker run -d \
+-p 9000:9000 \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v $portainerDir:/data \
+--restart always \
+--name portainer \
+portainer/portainer
