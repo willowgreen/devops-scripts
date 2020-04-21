979e9dcf2d6eb28c2b52e8d579963a5183719367
Nick Schlobohm <nschlobohm@willowgreengroup.com.au>
2020-04-21 19:48:18 +1000

    Create cassandra.sh

---
+++ docker/containers/cassandra.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# willowgreengroup/devops-scripts
+# https://github.com/willowgreengroup/devops-scripts/
+cassandraDir="/srv/cassandra"
+mkdir -p $cassandraDir
+sudo docker run -d -p 9042:9042 -v $cassandraDir:/var/lib/cassandra --restart always --name cassandra cassandra
