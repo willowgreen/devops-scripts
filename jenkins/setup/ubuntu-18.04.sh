1a32a5224a7f219dbb8a014e1a8414412dce427b
Nick Schlobohm <nschlobohm@willowgreengroup.com.au>
2020-04-24 09:54:30 +1000

    Split Java and Jenkins installs onto multiple lines for safety

--- jenkins/setup/ubuntu-18.04.sh
+++ jenkins/setup/ubuntu-18.04.sh
@@ -3,7 +3,8 @@
 # https://github.com/willowgreengroup/devops-scripts/
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update && sudo apt install -y openjdk-8-jdk jenkins
+sudo apt update && sudo apt install -y openjdk-8-jdk
+sudo apt install jenkins
 sudo systemctl start jenkins
 sudo service jenkins start
 echo "Initial admin password:"
