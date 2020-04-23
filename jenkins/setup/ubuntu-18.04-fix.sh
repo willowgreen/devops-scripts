754c69c358589638bf242dc64aa235f5f1261967
Nick Schlobohm <nschlobohm@willowgreengroup.com.au>
2020-04-24 08:51:10 +1000

    Create ubuntu-18.04-fix.sh

---
+++ jenkins/setup/ubuntu-18.04-fix.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# willowgreengroup/devops-scripts
+# https://github.com/willowgreengroup/devops-scripts/
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9B7D32F2D50582E6
+chmod +x ubuntu-18.04.sh
+sh ./ubuntu-18.04.sh
