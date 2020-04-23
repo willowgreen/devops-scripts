5675ad2640bdfc566a33316e628a2e12230253f9
Nick Schlobohm <nschlobohm@willowgreengroup.com.au>
2020-04-23 23:28:18 +1000

    Create ubuntu-18.04.sh

---
+++ jenkins/setup/ubuntu-18.04.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# willowgreengroup/devops-scripts
+# https://github.com/willowgreengroup/devops-scripts/
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update && sudo apt install -y jenkins
+sudo systemctl start jenkins
+echo "Initial admin password:"
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
