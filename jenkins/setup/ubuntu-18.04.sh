a4ad875b6cd14ac1129b5d84dcd22a45c03a3cf7
Nick Schlobohm <nschlobohm@willowgreengroup.com.au>
2020-04-24 08:52:33 +1000

    Make sure to install Java before Jenkins

--- jenkins/setup/ubuntu-18.04.sh
+++ jenkins/setup/ubuntu-18.04.sh
@@ -3,7 +3,7 @@
 # https://github.com/willowgreengroup/devops-scripts/
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update && sudo apt install -y jenkins
+sudo apt update && sudo apt install -y openjdk-8-jdk jenkins
 sudo systemctl start jenkins
 echo "Initial admin password:"
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
